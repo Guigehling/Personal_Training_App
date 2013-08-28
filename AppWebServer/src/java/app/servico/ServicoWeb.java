54e419fb845e75654faadc3ca297b28fff1127f6
Guilherme Gehling <Guilherme Gehling@10.13.4.24>
2013-08-28 00:47:13 -0300

    Teste, refazer a classe do servlet, verificar os links para fazer a conexão.

---
+++ AppWebServer/src/java/app/servico/ServicoWeb.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.servico;
+
+import app.bean.ObjTeste;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Guilherme Gehling
+ */
+@Path(value = "/servicoweb")
+public class ServicoWeb {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String testeJSON(){
+        return "Funcionou";
+    }
+}
