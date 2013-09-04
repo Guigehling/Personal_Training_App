905ebb191030d4aec7fe19932671d17efd7e5fe7
Guilherme Gehling <Guilherme Gehling@10.13.4.24>
2013-09-03 22:48:27 -0300

    Testar a conexão utlizando a biblioteca adiquirida.

--- AppPersonalTraining/src/app/projeto/PersonalTraining.java
+++ AppPersonalTraining/src/app/projeto/PersonalTraining.java
@@ -2,13 +2,11 @@ package app.projeto;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import app.servico.ServicoWebClient;
 
 public class PersonalTraining extends Activity {
+
+    private ServicoWebClient servicoWeb;
 
     /**
      * Called when the activity is first created.
@@ -17,34 +15,6 @@ public class PersonalTraining extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-
-    public void onClickBtEnviar(View v) throws InterruptedException {
-        Thread t = new Thread(new ConexaoWWW());
-        t.start();
-    }
-
-    private class ConexaoWWW implements Runnable {
-
-        public void run() {
-            try {
-                URL urlObj = new URL("http://192.168.0.100:8080/TrabMsgWeb/ServletMsg");
-                HttpURLConnection httpConn = (HttpURLConnection) urlObj.openConnection();
-                httpConn.setDoInput(true);
-                httpConn.setDoOutput(true);
-                httpConn.setUseCaches(false);
-                httpConn.setRequestMethod("POST");
-                httpConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                httpConn.setRequestProperty("Content-Language", "pt-BR");
-                httpConn.setRequestProperty("Accept", "application/octet-stream");
-                httpConn.setRequestProperty("Connection", "close");
-            } catch (Exception ex) {
-//                Display display = Display.getDisplay(mid);
-//                Alert alert = new Alert("Informação");
-//                alert.setString("Erro na conexão!");
-//                display.setCurrent(alert);
-                ex.printStackTrace();
-            }
-        }
+        //  servicoWeb = new ServicoWebClient();
     }
 }
--- AppPersonalTraining/src/app/servico/ServicoWebClient.java
+++ AppPersonalTraining/src/app/servico/ServicoWebClient.java
@@ -4,9 +4,32 @@
  */
 package app.servico;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
 /**
  *
  * @author Guilherme Gehling
  */
 public class ServicoWebClient {
+
+    private WebResource webResource;
+    private Client client;
+    private static final String URL = "";
+
+    public ServicoWebClient() {
+        com.sun.jersey.api.client.config.ClientConfig config = new DefaultClientConfig();
+        client = Client.create(config);
+        webResource = client.resource(URL).path("servicoweb");
+    }
+
+    public String GetJASON() throws UniformInterfaceException {
+        return webResource.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(String.class);
+    }
+
+    public void close() {
+        client.destroy();
+    }
 }
