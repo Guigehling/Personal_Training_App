b352aaa156ecc67542869673d188b66e9017d081
Guilherme Gehling <Guilherme Gehling@10.13.4.24>
2013-10-02 01:43:16 -0300

    Conexao App - Web Server, esta funcionando e carregando a distancia por xml.

--- AppWebServer/src/java/app/servico/ServicoWeb.java
+++ AppWebServer/src/java/app/servico/ServicoWeb.java
@@ -6,9 +6,12 @@ package app.servico;
 
 import app.bean.Entidade;
 import app.bean.LatLong;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.URL;
@@ -24,8 +27,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import net.sf.json.JSONObject;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 /**
  *
@@ -63,10 +71,10 @@ public class ServicoWeb {
     @Produces(MediaType.APPLICATION_JSON)
     public LatLong postLatLong(LatLong latlong) throws IOException {
         StringBuilder sb = new StringBuilder("Registros Recebidos ------- \n\n");
-        conexaoGoogle();
         contGoogle();
-        ConexaoJSon();
-        latlong.setDistancia("12345");
+        //ConexaoJSon();
+        latlong.setDistancia(contGoogle());
+        //        conexaoGoogle();
         //String resp = String.format("LatLong [%s]", latlong.getLatitude_inicial());
         //sb.append(resp);
         //sb.append("\n");
@@ -77,15 +85,12 @@ public class ServicoWeb {
     //Faz a conexão com o Google
     public String contGoogle() {
         String distancia = "";
-        String urlString = "http://maps.googleapis.com/maps/api/directions/json?sensor=false&origin=-31.37392-51.997007&destination=-31.375294-51.955765&sensor=false";
-        //http://maps.googleapis.com/maps/api/directions/json?sensor=false&origin=-31.37392-51.997007&destination=-31.375294-51.955765&sensor=false
+        String urlString = "http://maps.googleapis.com/maps/api/directions/xml?origin=-31.367333,-51.982088&destination=-31.367276,-51.981925&sensor=false";
         System.out.println(urlString);
 
         try {
             URL urlGoogleDirService = new URL(urlString);
-
             HttpURLConnection urlGoogleDirCon = (HttpURLConnection) urlGoogleDirService.openConnection();
-
             urlGoogleDirCon.setAllowUserInteraction(false);
             urlGoogleDirCon.setDoInput(true);
             urlGoogleDirCon.setDoOutput(false);
@@ -94,18 +99,29 @@ public class ServicoWeb {
             urlGoogleDirCon.connect();
 
             BufferedReader in = new BufferedReader(new InputStreamReader(urlGoogleDirCon.getInputStream()));
+            String t = in.readLine();
+            System.out.println(t);
             String line = null;
+            String teste = "";
             while ((line = in.readLine()) != null) {
                 System.out.println(line);
+                teste = teste + line;
             }
-            in.close();
 
-            DocumentBuilderFactory factoryDir = DocumentBuilderFactory.newInstance();
-            DocumentBuilder parserDirInfo = factoryDir.newDocumentBuilder();
-            Document docDir = parserDirInfo.parse(urlGoogleDirCon.getInputStream());
-
+            XPathFactory factory = XPathFactory.newInstance();
+            XPath xPath = factory.newXPath();
+            String espressao = "/DirectionsResponse/route/leg/distance/value";
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = null;
+            builder = builderFactory.newDocumentBuilder();
+            Document xmlDocument = builder.parse(new ByteArrayInputStream(teste.getBytes()));
+            NodeList nodeList = (NodeList) xPath.compile(espressao).evaluate(xmlDocument, XPathConstants.NODESET);
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                System.out.println(nodeList.item(i).getFirstChild().getNodeValue());
+                distancia = nodeList.item(i).getFirstChild().getNodeValue();
+            }
+            
             urlGoogleDirCon.disconnect();
-            distancia = docDir.toString();
             return distancia;
         } catch (Exception e) {
             System.out.println(e);
